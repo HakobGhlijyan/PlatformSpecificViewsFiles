64ab22eef805eb6fd89e1cd27ce6baffd176fc75
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-12-17 16:53:29 +0400

    Initial Commit

---
+++ PlatformSpecificViewsFiles/PlatformSpecificViewsFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatformSpecificViewsFilesApp.swift
+//  PlatformSpecificViewsFiles
+//
+//  Created by Hakob Ghlijyan on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PlatformSpecificViewsFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
